5bc6cc0d6e914c3100e2d9f160e6ed410e7aa3f2
Antonio Scandurra <as-cii@github.com>
2017-06-06 11:21:44 +0200

    WIP

---
+++ index.js
@@ -0,0 +1,2 @@
+const RealTimePackage = require('./lib/real-time-package')
+module.exports = new RealTimePackage()
---
+++ test/buffer-binding.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert')
+
+describe('asd', () => {
+  it('foo', () => {
+    assert.equal('a', 'b')
+  })
+})
