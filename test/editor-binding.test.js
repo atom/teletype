92ee89e64536ec52bf094fd1bc68ec06d68c1bab
Antonio Scandurra <as-cii@github.com>
2017-06-13 10:40:18 +0200

    Use a unit test to capture the behavior of EditorBinding

---
+++ test/editor-binding.test.js
@@ -0,0 +1,103 @@
+const assert = require('assert')
+const fs = require('fs')
+const path = require('path')
+const SAMPLE_TEXT = fs.readFileSync(path.join(__dirname, 'fixtures', 'sample.js'), 'utf8')
+const {TextEditor} = require('atom')
+const EditorBinding = require('../lib/editor-binding')
+
+describe('EditorBinding', () => {
+  it('relays local selections and creates cursor decorations on the local editor based on the remote ones', () => {
+    const editor = new TextEditor()
+    editor.setText(SAMPLE_TEXT)
+    editor.setCursorBufferPosition([0, 0])
+
+    const binding = new EditorBinding(editor)
+    const sharedEditor = new FakeSharedEditor(binding)
+    binding.setSharedEditor(sharedEditor)
+    assert.deepEqual(
+      sharedEditor.rangesByMarkerId,
+      {
+        1: {start: {row: 0, column: 0}, end: {row: 0, column: 0}}
+      }
+    )
+
+    editor.setSelectedBufferRanges([
+      [[10, 0], [11, 4]],
+      [[20, 0], [20, 5]]
+    ])
+    assert.deepEqual(
+      sharedEditor.rangesByMarkerId,
+      {
+        1: {start: {row: 10, column: 0}, end: {row: 11, column: 4}},
+        2: {start: {row: 20, column: 0}, end: {row: 20, column: 5}}
+      }
+    )
+
+    binding.setSelectionMarkerLayerForSiteId(2, {
+      1: {start: {row: 3, column: 0}, end: {row: 4, column: 2}},
+      2: {start: {row: 5, column: 0}, end: {row: 6, column: 0}}
+    })
+    assert.deepEqual(
+      getCursorDecoratedRanges(editor),
+      [
+        {start: {row: 3, column: 0}, end: {row: 4, column: 2}},
+        {start: {row: 5, column: 0}, end: {row: 6, column: 0}},
+        {start: {row: 10, column: 0}, end: {row: 11, column: 4}},
+        {start: {row: 20, column: 0}, end: {row: 20, column: 5}}
+      ]
+    )
+  })
+
+  it('updates the scroll position based on the position of the last cursor on the host', () => {
+    const editor = new TextEditor()
+    editor.setText(SAMPLE_TEXT)
+    editor.setCursorBufferPosition([0, 0])
+
+    const binding = new EditorBinding(editor)
+    binding.setSharedEditor(new FakeSharedEditor(binding))
+
+    const scrollRequests = []
+    editor.onDidRequestAutoscroll(({screenRange}) => scrollRequests.push(screenRange))
+
+    binding.setSelectionMarkerLayerForSiteId(1, {
+      1: {start: {row: 3, column: 0}, end: {row: 4, column: 2}},
+      2: {start: {row: 5, column: 0}, end: {row: 6, column: 1}}
+    })
+    assert.deepEqual(scrollRequests, [{start: {row: 5, column: 0}, end: {row: 6, column: 0}}])
+
+    scrollRequests.length = 0
+    binding.setSelectionMarkerLayerForSiteId(1, {
+      1: {start: {row: 3, column: 0}, end: {row: 4, column: 2}},
+      2: {start: {row: 5, column: 0}, end: {row: 6, column: 1}},
+      3: {start: {row: 1, column: 0}, end: {row: 1, column: 3}}
+    })
+    assert.deepEqual(scrollRequests, [{start: {row: 1, column: 0}, end: {row: 1, column: 3}}])
+
+    scrollRequests.length = 0
+    binding.setSelectionMarkerLayerForSiteId(2, {
+      1: {start: {row: 10, column: 0}, end: {row: 10, column: 2}}
+    })
+    assert.deepEqual(scrollRequests, [])
+  })
+
+  function getCursorDecoratedRanges (editor) {
+    const {decorationManager} = editor
+    const decorationsByMarker = decorationManager.decorationPropertiesByMarkerForScreenRowRange(0, Infinity)
+    const ranges = []
+    for (const [marker, decorations] of decorationsByMarker) {
+      const hasCursorDecoration = decorations.some((d) => d.type === 'cursor')
+      if (hasCursorDecoration) ranges.push(marker.getBufferRange())
+    }
+    return ranges.sort((a, b) => a.compare(b))
+  }
+})
+
+class FakeSharedEditor {
+  constructor (delegate) {
+    this.delegate = delegate
+  }
+
+  setSelectionRanges (rangesByMarkerId) {
+    this.rangesByMarkerId = rangesByMarkerId
+  }
+}
--- test/real-time-package.test.js
+++ test/real-time-package.test.js
@@ -36,7 +36,7 @@ suite('RealTimePackage', () => {
     containerElement.remove()
   })
 
-  test('sharing and joining buffers', async function () {
+  test('sharing and joining editors', async function () {
     let clipboardText
     const env1 = buildAtomEnvironment()
     const env1Package = new RealTimePackage({
@@ -86,73 +86,6 @@ suite('RealTimePackage', () => {
       [[0, 5], [0, 7]]
     ])
     await condition(() => deepEqual(getCursorDecoratedRanges(env1Editor), getCursorDecoratedRanges(env2Editor)))
-
-    env1Editor.setSelectedBufferRange([[0, 0], [0, 2]])
-    env2Editor.setSelectedBufferRange([[0, 5], [0, 7]])
-    await condition(() => deepEqual(getCursorDecoratedRanges(env1Editor), getCursorDecoratedRanges(env2Editor)))
-  })
-
-  test('scroll position is synced from host to guest', async function () {
-    let clipboardText
-    const env1 = buildAtomEnvironment()
-    const env1Package = new RealTimePackage({
-      restGateway: testServer.restGateway,
-      pubSubGateway: testServer.pubSubGateway,
-      workspace: env1.workspace,
-      commands: env1.commands,
-      clipboard: {
-        write (text) {
-          clipboardText = text
-        }
-      }
-    })
-
-    const env2 = buildAtomEnvironment()
-    const env2Package = new RealTimePackage({
-      restGateway: testServer.restGateway,
-      pubSubGateway: testServer.pubSubGateway,
-      workspace: env2.workspace,
-      commands: env2.commands,
-      clipboard: {
-        read () {
-          return clipboardText
-        }
-      }
-    })
-
-    const env1Editor = await env1.workspace.open(temp.path({extension: '.js'}))
-    env1Editor.setText(fs.readFileSync(path.join(__dirname, 'fixtures', 'sample.js'), 'utf8'))
-    await env1Package.shareEditor(env1Editor)
-    await env2Package.joinEditor(clipboardText)
-    const env2Editor = env2.workspace.getActiveTextEditor()
-    await condition(() => deepEqual(getCursorDecoratedRanges(env1Editor), getCursorDecoratedRanges(env2Editor)))
-
-    const maxVisibleRows = 5
-    const env1WorkspaceElement = env1.workspace.getElement()
-    containerElement.appendChild(env1WorkspaceElement)
-    env1WorkspaceElement.style.height = maxVisibleRows * env1Editor.getLineHeightInPixels() + 'px'
-
-    const env2WorkspaceElement = env2.workspace.getElement()
-    containerElement.appendChild(env2WorkspaceElement)
-    env2WorkspaceElement.style.height = maxVisibleRows * env2Editor.getLineHeightInPixels() + 'px'
-
-    let env2PreviousScrollTopRow = env2Editor.getScrollTopRow()
-    env1Editor.setCursorBufferPosition([10, 0])
-    await condition(() => deepEqual(getCursorDecoratedRanges(env1Editor), getCursorDecoratedRanges(env2Editor)))
-    await condition(() => env2Editor.getScrollTopRow() != env2PreviousScrollTopRow)
-    assert(env2Editor.getFirstVisibleScreenRow() < 10 && 10 < env2Editor.getLastVisibleScreenRow())
-
-    const env1PreviousScrollTopRow = env1Editor.getScrollTopRow()
-    env2Editor.setCursorBufferPosition([20, 0])
-    await condition(() => deepEqual(getCursorDecoratedRanges(env1Editor), getCursorDecoratedRanges(env2Editor)))
-    assert.equal(env1Editor.getScrollTopRow(), env1PreviousScrollTopRow)
-
-    // TODO: Consider moving this into its own test
-    env2PreviousScrollTopRow = env2Editor.getScrollTopRow()
-    env1Editor.addCursorAtBufferPosition([30, 0])
-    await condition(() => deepEqual(getCursorDecoratedRanges(env1Editor), getCursorDecoratedRanges(env2Editor)))
-    await condition(() => env2Editor.getScrollTopRow() != env2PreviousScrollTopRow)
-    assert(env2Editor.getFirstVisibleScreenRow() < 30 && 30 < env2Editor.getLastVisibleScreenRow())
   })
 })
 
