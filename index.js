f794c0ee3dce07281231a93323a4da8a6ffe863e
Jason Rudolph <jason@jasonrudolph.com>
2017-06-22 16:08:50 -0400

    :art:

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const RealTimePackage = require('./lib/real-time-package')
 module.exports = new RealTimePackage({
   workspace: atom.workspace,
+  notificationManager: atom.notifications,
   commandRegistry: atom.commands,
   clipboard: atom.clipboard,
   pusherKey: process.env.REAL_TIME_PUSHER_KEY,
