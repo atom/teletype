fa93f4292d2dac2b5b3f40fbdffad18dcfcf5872
Antonio Scandurra <as-cii@github.com>
2017-06-23 16:06:09 +0200

    Use production pusher key and base URL by default
    ...and log them out, too.
    
    Signed-off-by: Jason Rudolph <jasonrudolph@github.com>

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const PRODUCTION_REAL_TIME_PUSHER_KEY = 'f119821248b7429bece3'
+const PRODUCTION_REAL_TIME_BASE_URL = 'https://atom-tachyon.herokuapp.com'
+
 const RealTimePackage = require('./lib/real-time-package')
 module.exports = new RealTimePackage({
   workspace: atom.workspace,
   notificationManager: atom.notifications,
   commandRegistry: atom.commands,
   clipboard: atom.clipboard,
-  pusherKey: process.env.REAL_TIME_PUSHER_KEY,
-  baseURL: process.env.REAL_TIME_BASE_URL
+  pusherKey: process.env.REAL_TIME_PUSHER_KEY || PRODUCTION_REAL_TIME_PUSHER_KEY,
+  baseURL: process.env.REAL_TIME_BASE_URL || PRODUCTION_REAL_TIME_BASE_URL
 })
