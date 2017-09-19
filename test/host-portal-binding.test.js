8963a1cdd07e6f58ab8be2cffe2925adefb07025
Nathan Sobo <nathan@github.com>
2017-09-19 12:30:17 -0600

    Catch and display exceptions when sharing a portal

---
+++ test/host-portal-binding.test.js
@@ -0,0 +1,31 @@
+const {RealTimeClient} = require('@atom/real-time-client')
+const assert = require('assert')
+const HostPortalBinding = require('../lib/host-portal-binding')
+
+const suite = global.describe
+const test = global.it
+
+suite('HostPortalBinding', () => {
+  test('handling an unexpected error when joining a portal', async () => {
+    const client = new RealTimeClient({})
+    client.createPortal = function () {
+      throw new Error('It broke!')
+    }
+    const notificationManager = {
+      errors: [],
+      addError (...args) { this.errors.push(args) }
+    }
+    const portalBinding = new HostPortalBinding({
+      client,
+      notificationManager
+    })
+
+    const result = await portalBinding.initialize()
+    assert.equal(result, false)
+
+    assert.equal(notificationManager.errors.length, 1)
+    const [[message, params]] = notificationManager.errors
+    assert.equal(message, 'Failed to share portal')
+    assert(params.description.includes('It broke!'))
+  })
+})
