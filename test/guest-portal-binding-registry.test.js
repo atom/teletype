caef9f092b132d99689ec736ce14af7bd40f7f48
Antonio Scandurra <as-cii@github.com>
2017-09-20 18:44:33 +0200

    Allow to create guest portal bindings again if failing the first time

--- test/guest-portal-binding-registry.test.js
+++ test/guest-portal-binding-registry.test.js
@@ -5,6 +5,62 @@ const suite = global.describe
 const test = global.it
 
 suite('GuestPortalBindingRegistry', () => {
+  test('fetching the same binding multiple times before it has been initialized', async () => {
+    const client = {
+      joinPortal (id) {
+        return new Promise(() => {})
+      }
+    }
+    const workspace = {
+      getElement () {
+        return document.createElement('div')
+      },
+      observeActivePaneItem () {}
+    }
+
+    const registry = new GuestPortalBindingRegistry({client, workspace})
+    const portal1BindingPromise1 = registry.getPortalBinding('1')
+    const portal1BindingPromise2 = registry.getPortalBinding('1')
+    const portal2BindingPromise1 = registry.getPortalBinding('2')
+    assert.equal(portal1BindingPromise1, portal1BindingPromise2)
+    assert.notEqual(portal1BindingPromise1, portal2BindingPromise1)
+  })
+
+  test('successfully fetching a binding after failing the first time', async () => {
+    let resolveLastJoinPortalPromise, rejectLastJoinPortalPromise
+    const client = {
+      joinPortal (id) {
+        return new Promise((resolve, reject) => {
+          resolveLastJoinPortalPromise = resolve
+          rejectLastJoinPortalPromise = reject
+        })
+      }
+    }
+    const workspace = {
+      getElement () {
+        return document.createElement('div')
+      },
+      observeActivePaneItem () {}
+    }
+    const notificationManager = {
+      addError () {}
+    }
+
+    const registry = new GuestPortalBindingRegistry({client, workspace, notificationManager})
+    const portalBinding1Promise1 = registry.getPortalBinding('1')
+    rejectLastJoinPortalPromise(new Error())
+    assert.equal(await portalBinding1Promise1, null)
+
+    const portalBinding1Promise2 = registry.getPortalBinding('1')
+    assert.notEqual(portalBinding1Promise1, portalBinding1Promise2)
+
+    const portal = {
+      setDelegate () {}
+    }
+    resolveLastJoinPortalPromise(portal)
+    assert.equal((await portalBinding1Promise2).portal, portal)
+  })
+
   test('adding and removing classes from the workspace element', async () => {
     const client = {
       joinPortal () {
