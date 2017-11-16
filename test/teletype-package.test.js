5bdddc58fe047a22ff9c1eb738beeab7e0571e3b
Wliu <50Wliu@users.noreply.github.com>
2017-11-16 22:08:32 +0100

    Try this approach instead

--- test/teletype-package.test.js
+++ test/teletype-package.test.js
@@ -975,7 +975,7 @@ suite('TeletypePackage', function () {
 
     if (options.signIn == null || options.signIn) {
       await credentialCache.set('oauth-token', 'token-' + nextTokenId++)
-      await pack.activate()
+      await pack.signInUsingSavedToken()
     }
     packages.push(pack)
     return pack
