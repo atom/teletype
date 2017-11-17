b96c9ed55d91b9887810bf9b3d89340504ce5e08
Wliu <50Wliu@users.noreply.github.com>
2017-11-17 20:04:00 +0100

    :art:

--- test/teletype-package.test.js
+++ test/teletype-package.test.js
@@ -884,7 +884,6 @@ suite('TeletypePackage', function () {
     {
       const env = buildAtomEnvironment()
       const pack = await buildPackage(env, {signIn: false})
-
       pack.client.initialize = async function () {
         throw new Error('an error')
       }
