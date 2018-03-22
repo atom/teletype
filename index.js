0eabac3f463b3491ea4c45195737560697819ad3
Jason Rudolph <jason@jasonrudolph.com>
2018-03-22 11:15:16 -0400

    Group Teletype's dev settings together and collapse them by default
    Co-authored-by: Antonio Scandurra <as-cii@github.com>

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ module.exports = new TeletypePackage({
   commandRegistry: atom.commands,
   tooltipManager: atom.tooltips,
   clipboard: atom.clipboard,
-  pusherKey: atom.config.get('teletype.pusherKey'),
+  pusherKey: atom.config.get('teletype.dev.pusherKey'),
   pusherOptions: {
-    cluster: atom.config.get('teletype.pusherCluster'),
+    cluster: atom.config.get('teletype.dev.pusherCluster'),
     disableStats: true
   },
-  baseURL: atom.config.get('teletype.baseURL'),
+  baseURL: atom.config.get('teletype.dev.baseURL'),
   getAtomVersion: atom.getVersion.bind(atom)
 })
