aa4e09e43687a625ec46fb456dd3e749b89c91d9
Musa Ibrahim <small_musa@hotmail.com>
2020-09-03 09:14:17 +0100

    fixed linting

--- test/helpers/condition.js
+++ test/helpers/condition.js
@@ -5,12 +5,12 @@ module.exports = function condition (fn, shouldLog = false) {
   return new Promise((resolve, reject) => {
     const intervalId = global.setInterval(async () => {
       let result = fn()
-      if(shouldLog) {
-        console.log({result}, fn.toString());
+      if (shouldLog) {
+        console.log({result}, fn.toString())
       }
       if (result instanceof Promise) {
         result = await result
-        console.log({result}, fn.toString());
+        console.log({result}, fn.toString())
       }
 
       if (result) {
