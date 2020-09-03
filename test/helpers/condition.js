9be84927c47d22b1bcc50c2a329d989f0aaff1df
Musa Ibrahim <small_musa@hotmail.com>
2020-09-03 09:25:10 +0100

    verify if promises resolve

--- test/helpers/condition.js
+++ test/helpers/condition.js
@@ -5,17 +5,19 @@ module.exports = function condition (fn, shouldLog = false) {
   return new Promise((resolve, reject) => {
     const intervalId = global.setInterval(async () => {
       let result = fn()
-      if (shouldLog) {
-        console.log({result}, fn.toString())
-      }
       if (result instanceof Promise) {
         result = await result
-        console.log({result}, fn.toString())
       }
 
       if (result) {
+        if(shouldLog) {
+          console.log('I got here');
+        }
         global.clearTimeout(timeout)
         global.clearInterval(intervalId)
+        if(shouldLog) {
+          console.log('I got here and resolved');
+        }
         resolve()
       }
     }, 5)
